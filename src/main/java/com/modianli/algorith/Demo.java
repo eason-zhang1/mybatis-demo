ec0ba2eb812d7e8755b6dfecfe13e7666f21267e
张海军 <739663407@qq.com>
2018-05-08 17:33:25 +0800

    数字统计； 同步

--- src/main/java/com/modianli/algorith/Demo.java
+++ src/main/java/com/modianli/algorith/Demo.java
@@ -3,11 +3,18 @@ package com.modianli.algorith;
 public class Demo {
 
   public static void main(String[] args) {
-	System.out.println("求和结果: " + abP(10, -20));
-	System.out.println("最大值: " + max(100, 125));
+	//System.out.println("求和结果: " + abP(10, -20));
+	//System.out.println(trailingZeros(10014848));
 
-	binaryStr(120);
-	System.out.println(trailingZeros(10014848));
+	System.out.println(digitCounts(1, 12));
+
+	System.out.println("===========================");
+
+	//synchronized 修饰的静态方法时， 等同于 synchronized(XX.class){}
+	//修饰非静态方法时, 等同于 synchronized(this){}
+	Demo demo = new Demo();
+	new Thread(() -> demo.testSynchronized1()).start();
+	new Thread(() -> demo.testSynchronized2()).start();
   }
 
   private static int abP(int a, int b) {
@@ -28,14 +35,6 @@ public class Demo {
 	return c;
   }
 
-  private static int max(int a, int b) {
-	return a ^ b;
-  }
-
-  private static void binaryStr(int a) {
-	System.out.println(Integer.toBinaryString(a));
-  }
-
   public static long trailingZeros(long n) {
 	// write your code here, try to do it without arithmetic operators.
 	Long count5 = 0L;
@@ -52,5 +51,53 @@ public class Demo {
 	}
 
 	return count5;
+  }
+
+  public static int digitCounts(int k, int n) {
+	if (k < 0 || k > 9) {
+	  throw new IllegalArgumentException("K值不在允许(0 ~ 9)范围");
+	}
+	if (n < 0) {
+	  throw new IllegalArgumentException("N值不在允许(N > 0)范围");
+	}
+	if (n < k) {
+	  return 0;
+	}
+
+	int count = 1;
+	for (int i = k + 1; i <= n; i++) {
+	  int tmp = i;
+	  while (tmp / 10 != 0) {
+		if (tmp % 10 == k) {
+		  count++;
+		}
+
+		tmp /= 10;
+	  }
+
+	  if (tmp == k) {
+		count++;
+	  }
+	}
+
+	return count;
+  }
+
+  public synchronized static void testSynchronized1() {
+	for (int i = 0; i < 20; i += 2) {
+	  System.out.println(i);
+	}
+
+	try {
+	  Thread.sleep(3000);
+	} catch (InterruptedException e) {
+	  e.printStackTrace();
+	}
+  }
+
+  public synchronized static void testSynchronized2() {
+	for (int i = 1; i < 20; i += 2) {
+	  System.out.println(i);
+	}
   }
 }
